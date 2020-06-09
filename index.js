895ce1e44889f1f31f6ade0d7a726f469c43e508
evilvn123 <46784967+evilvn123@users.noreply.github.com>
2020-06-10 01:54:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var crypto = require('crypto');
-var uuid = require('uuid');
 var express = require('express');
 var mysql = require('mysql');
-var bodyParser = require('body-parser');
 var app = express();
-var fs = require("fs");
+const moment = require('moment-timezone');
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -18,202 +15,41 @@ var conn = mysql.createConnection({
       host    : 'bvfopzmpe3wvyhzvzip2-mysql.services.clever-cloud.com',
       user    : 'uo2i94d6ddzrqmqy',
       password: 'n4ZKdalk4QVP3KqgfdFc',
-      database: 'bvfopzmpe3wvyhzvzip2',
+	  database: 'bvfopzmpe3wvyhzvzip2'
 });
+let sql0 = 'CREATE TABLE IF NOT EXISTS data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, temp INT(10), gas INT(10), time DATETIME) ENGINE = InnoDB' ;
 
-/*---------------------------------------------------------------------------*/
-//Giang
-var ketqua;
-//password 
-var getRandomString =function(length){
-	return crypto.randomBytes(Math.ceil(length/2))
-	.toString('hex') // convert to hexa
-	.slice(0,length); // return required number of char
-};
-
-var sha512 = function(password,salt){
-	var hash = crypto.createHmac('sha512',salt); 
-	hash.update(password);
-	var value = hash.digest('hex');
-	return{
-		salt:salt,
-		passwordHash:value
-	};
-};
-
-function saltHashPassword(userPassword){
-var salt = getRandomString(16);
-var passwordData = sha512(userPassword,salt);
-return passwordData;
-};
-
-function checkHashPassword(userPassword,salt){
-
-	var passwordData = sha512(userPassword,salt);
-	return passwordData;
-
-};
-
-
-io.sockets.on('connection', function(socket){
-
-console.log("co nguoi ket noi ");
-// dang ki tai khoan
-socket.on('client-dang-ki-user', function(data){
-	//var ketqua;
-	var name     		= data.name;
-	var email    		= data.email;
-	var password 		= data.password;
-	var uid 			= uuid.v4();
-	var plaint_password = password;
-	var hash_data 		= saltHashPassword(plaint_password);
-	var password  		= hash_data.passwordHash;
-	var salt 			= hash_data.salt;
-	
-  
- conn.query('SELECT * FROM user where email=?',[email], function(err,result, fields){
-		conn.on('error',function(err){
-			console.log('mysql error',err);
-		});
-
-		if (result && result.length){
-			ketqua = false;
-		
-		console.log("tai khoan da ton tai ");
-	}
-	else{
-		ketqua = true;
-		conn.query('INSERT INTO `user`(`unique_id`, `name`, `email`, `encrypted_password`, `salt`, `create_at`, `updated_at`) VALUES (?,?,?,?,?,NOW(),NOW())',[uid,name,email,password,salt],function(err,result, fields){
-		conn.on('error',function(err){
-			console.log('mysql error',err);
-			console.log('khong thanh cong');
-			
-		});
-		console.log('thanh cong');
-		//socket.emit('ket-qua-dang-ki',{noidung: ketqua});
-	});
-	}
-	 socket.emit('ket-qua-dang-ki',{noidung: ketqua});
-	});	
-
-});
-// dang nhap
-socket.on('client-dang-nhap-user', function(data){
-	var email    	  = data.email;
-	var user_password = data.password;
-	
- conn.query('SELECT * FROM user where email=?',[email], function(err,result, fields){
-		conn.on('error',function(err){
-			console.log('mysql error',err);
-		});
-		if (result && result.length)
-		{
-			var salt = result[0].salt;
-			var encrypted_password = result[0].encrypted_password;
-			var hashed_password = checkHashPassword(user_password,salt).passwordHash.slice(0,16);
-			if (encrypted_password == hashed_password) {
-				ketqua = true;
-				//res.end(JSON.stringify(result[0]));
-				console.log('dang nhap thanh cong');
-				console.log(result[0]);
-				socket.join(result[0].unique_id);
-			}
-			else{
-				ketqua = false;
-				console.log('dang nhap k thanh cong');
-			}
-			
-		 }
-		
-	else
-	{
-		ketqua = false;
-	console.log('dang nhap k thanh cong');
-	}
-	socket.emit('ket-qua-dang-nhap',{noidung: ketqua});
-	});
-});
-//gui du lieu device
-// socket.on('client-gui-device', function(data){
-// 	device_id = data.device_id;
-// 	unique_id = data.unique_id;
-//   	name      = data.name;
-//   	mode      = data.mode;
-//   	speed     = data.speed;
-//   	rotation  = data.rotation;
-  
-//   con.query('UPDATE `devices` SET `device_id`=?,`unique_id`=?,`name`=?,`mode`=?,`speed`=?,`rotation`=? WHERE 1',[device_id,unique_id,name,mode,speed,rotation], function(err,result, fields){
-// 		con.on('error',function(err){
-// 			console.log('mysql error',err);
-// 		});
-
-	
-// 	});	
-
-
-
-// });
-
-
-});
-//end Giang
-/*-------------------------------------------------------------------------------*/
-io.on('connection', function (socket) { //Bắt sự kiện một client kết nối đến server
-  console.log(socket.id + " connected");
-
-
-  //device
-  socket.join("control");
-  conn.connect(function (err){
-    //if (err) throw err.stack;
-    console.log("ket noi thanh cong");
-    let sql0 = `CREATE TABLE IF NOT EXISTS device1_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
-      //console.log(sql0);
-    conn.query(sql0, function (err) {
-        //if (err) throw err;
+io.on('connection', function(socket){
+	console.log('a user connected');
+	conn.query(sql0, function (err) {
         conn.on('error',function(err){
           console.log('mysql error',err);
         });       
         console.log('Tao bang thanh cong');
     });
-  });
-  socket.on('mode', function(data){
-  	console.log("nhan mode");
-  	io.sockets.in('control').emit('control_mode', data);
-  });
-  socket.on('client-gui-user', function(data){
-  	console.log("nhan client-gui-user");
-  });  
-  socket.on('update_data', function (data) { //thông số động cơ
-    //socket.broadcast.emit('news', data); 
-    console.log("nhan update");
-    var data_json = JSON.stringify(data)
-    console.log(".");
-    console.log(data);
-    console.log("devide id: " + data.device_id);
-    console.log("tocdo: " + data.tocdo);
-    console.log("chieuquay: " + data.chieuquay);
-    conn.connect(function (err){
-    //if (err) throw err.stack;
-      //nếu thành công
-      let sql0 = `CREATE TABLE IF NOT EXISTS device${data.device_id}_log (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,ThoiGian DATETIME DEFAULT CURTIME(), chieuquay VARCHAR(255), tocdo INT(10)) ENGINE = InnoDB` ;
-      //console.log(sql0);
-      conn.query(sql0, function (err) {
-          //if (err) throw err;
-          //console.log('Tao bang thanh cong');
-      });
-      let sql1 = `INSERT INTO device${data.device_id}_log(chieuquay, tocdo) values (  \'${data.chieuquay}\', \'${data.tocdo}\')` ;
-      //console.log(sql1);
-      conn.query(sql1, function (err) {
-          //if (err) throw err;
-          //console.log('Thay doi thanh cong');
-      });
-    });
-  });
 
-  //app
+	socket.on('disconnect', function(){
+	  console.log('user disconnected');
+	});
 
+	socket.on('data', function(data){
+		console.log('message: ' + data);
+		var now= moment();
+		let sql1 = `INSERT INTO data (temp, gas) values (?,?,?)` ;
+		
+		var date=now.tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD');
+		var time=now.tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss');
+		let todo = [data.temp, data.gas,time];
+		conn.query(sql1, todo, (err, results, fields) => {
+			if (err) {
+			  return console.error(err.message);
+			}
+			// get inserted id
+			console.log('Todo Id:' + results.insertId);
+		  });
+	  });
 });
+
 
 app.get("/admin", function(req, res){
 	res.render("trangchu");
