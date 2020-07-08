bf8767615cbbbaa319b5d187c8363a89c0aa4342
evilvn123 <46784967+evilvn123@users.noreply.github.com>
2020-07-08 12:05:54 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -18,13 +18,17 @@ var conn = mysql.createConnection({
       database: 'bvfopzmpe3wvyhzvzip2',
 });
 
-//handleDisconnect();
-let sql0 = 'CREATE TABLE IF NOT EXISTS data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, temp INT(10), gas INT(10), time TIMESTAMP) ENGINE = InnoDB' ;
+handleDisconnect();
+let sql0 = 'CREATE TABLE IF NOT EXISTS data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, temp INT(10), gas INT(10), time TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE = InnoDB' ;
 
 io.on('connection', function(socket){
 	console.log('a user connected');
 
 	io.sockets.emit('server-send-data', {content: 'This content'});
+	// socket.on('client-send-data', function(data){
+	// 	console.log(data);
+	// 	io.sockets.emit('server-send-data', {content: data});
+	// });
 
 
 	conn.query(sql0, function (err) {
@@ -37,12 +41,6 @@ io.on('connection', function(socket){
 	socket.on('disconnect', function(){
 	  console.log('user disconnected');
 	});
-	// socket.on('client-need-data', function(data){
-	// 	let sql = 'SELECT * FROM data';
-	// 	conn.query(sql, (err, results)=>{
-	// 		console.log(results.length)
-	// 	})
-	// });
 
 	socket.on('client-send-data', function(data){
 		var data_json = JSON.stringify(data)
@@ -50,7 +48,6 @@ io.on('connection', function(socket){
 		var now= moment();
 		let sql1 = `INSERT INTO data (temp, gas, time) values (?,?,?)` ;
 		var time=now.tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss');
-		//var date=now.tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD').toString();
 		console.log(time);
 		let todo = [data_json, data_json, time];
 		conn.query(sql1, todo, (err, results, fields) => {
@@ -58,21 +55,8 @@ io.on('connection', function(socket){
 			  return console.error(err.message);
 			}
 			console.log('Todo Id:' + results.insertId);
-		});
+		  });
 		io.sockets.emit('server-send-data', {content: data});
-		// con.query('SELECT COUNT(*) AS so_luong FROM data', function(err,result, fields){
-		// 	con.on('error',function(err){
-		// 		console.log('mysql error 179',err);
-		// 	});
-		// 	if (result.so_luong > 50000){
-		// 		con.query('DELETE FROM data', function(err,result, fields){
-		// 			con.on('error',function(err){
-		// 					console.log('mysql error 184',err);
-		// 			});			
-		// 		});
-		// 	}
-		// });
-		
 	  });
 });
 
@@ -81,27 +65,27 @@ app.get("/admin", function(req, res){
 	res.render("trangchu");
 })
 
-// function handleDisconnect() {
-// 	conn = mysql.createConnection({
-// 		host    : 'bvfopzmpe3wvyhzvzip2-mysql.services.clever-cloud.com',
-// 		user    : 'uo2i94d6ddzrqmqy',
-// 		password: 'n4ZKdalk4QVP3KqgfdFc',
-// 		database: 'bvfopzmpe3wvyhzvzip2',
-//     });
+function handleDisconnect() {
+	conn = mysql.createConnection({
+		host    : 'bvfopzmpe3wvyhzvzip2-mysql.services.clever-cloud.com',
+		user    : 'uo2i94d6ddzrqmqy',
+		password: 'n4ZKdalk4QVP3KqgfdFc',
+		database: 'bvfopzmpe3wvyhzvzip2',
+   });
 
-//     conn.connect(function(err) {             
-// 	   if(err) {                                     
-// 		 console.log('error when connecting to db:', err);
-// 		 setTimeout(handleDisconnect, 2000); 
-// 	   }                                  
-//     });                                     
-//     conn.on('error', function(err) {
-// 	   console.log('db error', err);
-// 	   if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-// 		 handleDisconnect();
-// 		 console.log("ket noi lai");                         
-// 	   } else {                                     
-// 		 throw err;                                  
-// 	   }
-//     });
-// }
+   conn.connect(function(err) {             
+	   if(err) {                                     
+		 console.log('error when connecting to db:', err);
+		 setTimeout(handleDisconnect, 2000); 
+	   }                                  
+   });                                     
+   conn.on('error', function(err) {
+	   console.log('db error', err);
+	   if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+		 handleDisconnect();
+		 console.log("ket noi lai");                         
+	   } else {                                     
+		 throw err;                                  
+	   }
+   });
+}
