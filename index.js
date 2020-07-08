c6d3048a260e6bc7fabcd614de21eead2ce1e98b
evilvn123 <46784967+evilvn123@users.noreply.github.com>
2020-07-08 11:17:52 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -19,16 +19,12 @@ var conn = mysql.createConnection({
 });
 
 handleDisconnect();
-let sql0 = 'CREATE TABLE IF NOT EXISTS data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, temp INT(10), gas INT(10), time TIMESTAMP DEFAULT CURRENT_TIMESTAMP) ENGINE = InnoDB' ;
+let sql0 = 'CREATE TABLE IF NOT EXISTS data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, temp INT(10), gas INT(10), time varchar(50), date varchar(50)) ENGINE = InnoDB' ;
 
 io.on('connection', function(socket){
 	console.log('a user connected');
 
 	io.sockets.emit('server-send-data', {content: 'This content'});
-	// socket.on('client-send-data', function(data){
-	// 	console.log(data);
-	// 	io.sockets.emit('server-send-data', {content: data});
-	// });
 
 
 	conn.query(sql0, function (err) {
@@ -41,21 +37,40 @@ io.on('connection', function(socket){
 	socket.on('disconnect', function(){
 	  console.log('user disconnected');
 	});
+	socket.on('client-need-data', function(data){
+		let sql = 'SELECT * FROM data';
+		conn.query(sql, (err, results)=>{
+			console.log(results.length)
+		})
+	});
 
 	socket.on('client-send-data', function(data){
 		var data_json = JSON.stringify(data)
 		console.log('message: ' + data_json);
 		var now= moment();
-		let sql1 = `INSERT INTO data (temp, gas, time) values (?,?,?)` ;
-		var time=now.tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss');
+		let sql1 = `INSERT INTO data (temp, gas, time, date) values (?,?,?,?)` ;
+		var time=now.tz('Asia/Ho_Chi_Minh').format('HH:mm:ss');
+		var date=now.tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD');
 		console.log(time);
-		let todo = [data_json, data_json, time];
+		let todo = [data_json, data_json, time, date];
 		conn.query(sql1, todo, (err, results, fields) => {
 			if (err) {
 			  return console.error(err.message);
 			}
 			console.log('Todo Id:' + results.insertId);
 		  });
+		con.query('SELECT COUNT(*) AS so_luong FROM data', function(err,result, fields){
+			con.on('error',function(err){
+				console.log('mysql error 179',err);
+			});
+			if (result.so_luong > 50000){
+				con.query('DELETE FROM data', function(err,result, fields){
+					con.on('error',function(err){
+							console.log('mysql error 184',err);
+					});			
+				});
+			}
+		});
 		io.sockets.emit('server-send-data', {content: data});
 	  });
 });
@@ -71,15 +86,15 @@ function handleDisconnect() {
 		user    : 'uo2i94d6ddzrqmqy',
 		password: 'n4ZKdalk4QVP3KqgfdFc',
 		database: 'bvfopzmpe3wvyhzvzip2',
-   });
+    });
 
-   conn.connect(function(err) {             
+    conn.connect(function(err) {             
 	   if(err) {                                     
 		 console.log('error when connecting to db:', err);
 		 setTimeout(handleDisconnect, 2000); 
 	   }                                  
-   });                                     
-   conn.on('error', function(err) {
+    });                                     
+    conn.on('error', function(err) {
 	   console.log('db error', err);
 	   if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
 		 handleDisconnect();
@@ -87,5 +102,5 @@ function handleDisconnect() {
 	   } else {                                     
 		 throw err;                                  
 	   }
-   });
+    });
 }
